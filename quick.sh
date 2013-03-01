e11bbc443a7a7de9e9240a1448d60c589725aa46
Chris Jacobs <zonk1024@gmail.com>
2013-02-28 22:18:05 -0800

    merged in master
     O.o

---
+++ quick.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+git config --global user.name "Chris Jacobs"
+git config --global user.email "zonk1024@gmail.com"
+git config --global color.ui auto
+# TY Scott Chacon
+git config --global alias.lol "log --oneline --decorate --graph"
+git config --global alias.ms "merge --no-ff --no-commit"
+git config --global alias.dmo "diff master --name-only"
+git config --global alias.dms "diff master --name-status"
+git config --global alias.cm "commit -am 'merged in master'"
