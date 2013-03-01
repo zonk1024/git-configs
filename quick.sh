ebba74f4d221a82fe4f0a4a383f336155f7ec902
Chris Jacobs <zonk1024@gmail.com>
2013-02-28 23:33:53 -0800

    few more

--- quick.sh
+++ quick.sh
@@ -6,6 +6,8 @@ git config --global color.ui auto
 # TY Scott Chacon
 git config --global alias.lol "log --oneline --decorate --graph"
 git config --global alias.ms "merge --no-ff --no-commit"
+git config --global alias.dm "diff master"
 git config --global alias.dmo "diff master --name-only"
 git config --global alias.dms "diff master --name-status"
 git config --global alias.cm "commit -am 'merged in master'"
+git config --global core.editor vi
