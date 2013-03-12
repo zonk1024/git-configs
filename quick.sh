d0a4fecb007c07ecfe10edfeee2e9ed2c9959a4e
Chris Jacobs <chrisj@parthenonsoftware.com>
2013-03-12 10:29:08 -0700

    added mm

--- quick.sh
+++ quick.sh
@@ -6,6 +6,7 @@ git config --global color.ui auto
 # TY Scott Chacon
 git config --global alias.lol "log --oneline --decorate --graph"
 git config --global alias.ms "merge --no-ff --no-commit"
+git config --global alias.mm "merge --no-ff --no-commit master"
 git config --global alias.dm "diff master"
 git config --global alias.dmo "diff master --name-only"
 git config --global alias.dms "diff master --name-status"
