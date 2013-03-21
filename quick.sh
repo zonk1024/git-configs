d4fc01dbd7a0417f2b9d63c229637765c93295ab
Chris Jacobs <zonk1024@gmail.com>
2013-03-21 16:22:27 -0700

    Added 'pom' for 'push origin master'

--- quick.sh
+++ quick.sh
@@ -6,6 +6,7 @@ git config --global color.ui auto
 # TY Scott Chacon
 git config --global alias.lol "log --oneline --decorate --graph"
 git config --global alias.po "push origin"
+git config --global alias.pom "push origin master"
 git config --global alias.ms "merge --no-ff --no-commit"
 git config --global alias.mm "merge --no-ff --no-commit master"
 git config --global alias.dm "diff master"
