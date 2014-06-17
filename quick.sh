094da06fcb1a6b9319b0eda3441dab916f1581ac
Chris Jacobs <zonk1024@gmail.com>
2014-06-17 11:07:51 -0700

    global gitignore

--- quick.sh
+++ quick.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
-
 git config --global user.name "Chris Jacobs"
 git config --global user.email "zonk1024@gmail.com"
 git config --global color.ui auto
-# TY Scott Chacon
-git config --global alias.lol "log --oneline --decorate --graph"
 git config --global alias.po "push origin"
 git config --global alias.pom "push origin master"
 git config --global alias.ms "merge --no-ff --no-commit"
@@ -15,9 +12,6 @@ git config --global alias.dms "diff master --name-status"
 git config --global alias.dh "diff HEAD"
 git config --global alias.dho "diff HEAD --name-only"
 git config --global alias.dhs "diff HEAD --name-status"
-git config --global alias.d2 "diff --color-words=. -w"
-# TY mackstann
-git config --global alias.cm "commit -am 'merged in master'"
 git config --global alias.f "fetch"
 git config --global alias.rh "reset --hard"
 git config --global alias.st "status"
@@ -25,5 +19,11 @@ git config --global alias.co "checkout"
 git config --global core.editor vi
 git config --global diff.tool vimdiff
 git config --global difftool.prompt false
-git config --global merge.conflictstyle diff3
+git config --global core.excludesfile "~/.gitignore"
+
+# TY Scott Chacon
+git config --global alias.lol "log --oneline --decorate --graph"
+
 # TY mackstann
+git config --global alias.d2 "diff --color-words=. -w"
+git config --global merge.conflictstyle diff3
